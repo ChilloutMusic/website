f4a71f13f378dc7dd71d9e1962ff0e7bc343d58d
braincomb <zapper@gmail.com>
2017-05-24 17:55:48 -0700

    Added Firebase and Firebase bindings.

--- client/app.js
+++ client/app.js
@@ -3,13 +3,20 @@ import { sync } from 'vuex-router-sync'
 import App from './components/App'
 import router from './router'
 import store from './store'
+import firebase from './service/firebase'
+import Vuefire from 'vuefire'
 
 sync(store, router)
 
+Vue.use(Vuefire)
+
 const app = new Vue({
+  firebase: {
+    rdjs: firebase.database.ref('rdjs').orderByChild('created_at')
+  },
   router,
   store,
   ...App
 })
 
-export { app, router, store }
+export { app, router, store, firebase }
---
+++ client/service/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+var config = {
+  apiKey: 'AIzaSyAmcCcvuccB8p_LNXmhwtl53uA9dp0x4-c',
+  authDomain: 'chilloutmusic-ba889.firebaseapp.com',
+  databaseURL: 'https://chilloutmusic-ba889.firebaseio.com',
+  storageBucket: 'chilloutmusic-ba889.appspot.com',
+  messagingSenderId: '812370035445'
+}
+
+firebase.initializeApp(config)
+
+export default {
+  database: firebase.database()
+}
