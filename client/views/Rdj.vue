886d0c36801dd38b8e0c25ddeced3d21dad16def
braincomb <zapper@gmail.com>
2017-05-25 20:07:07 -0700

    Some layout and data work on Rdj page.

--- client/views/Rdj.vue
+++ client/views/Rdj.vue
@@ -17,7 +17,24 @@
     <section class="hero">
       <div class="hero-body">
         <div class="container">
-          <p>content here</p>
+          <div class="columns">
+            <div class="column is-2" v-for="rdj in rdjs">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-200x200">
+                    <img :src="rdj.imageUrl" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title name">{{ rdj.name }}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -25,13 +42,33 @@
 </template>
 
 <script>
+import {db} from '../service/firebase'
+
 export default {
-  name: 'Rdj'
+  name: 'Rdj',
+  firebase: {
+    rdjs: db.ref('rdjs')
+  },
+  beforeCreate () {
+  }
 }
 </script>
 
 <style scoped>
 .page-rdj {
   background-color: #bec1ff;
+}
+
+.is-200x200 {
+  width: 100%;
+  height: 200px;
+}
+
+.card-content {
+  padding: 0.5rem;
+}
+
+.title.name {
+  font-size: 1.1rem;
 }
 </style>
