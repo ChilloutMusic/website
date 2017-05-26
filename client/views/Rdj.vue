1bd17bbb57250b450d0eb275d5da07ae31422a29
braincomb <zapper@gmail.com>
2017-05-26 00:04:29 -0700

    Make columns multiline. Display location and social linking.

--- client/views/Rdj.vue
+++ client/views/Rdj.vue
@@ -17,22 +17,25 @@
     <section class="hero">
       <div class="hero-body">
         <div class="container">
-          <div class="columns">
+          <div class="columns is-multiline">
             <div class="column is-2" v-for="rdj in rdjs">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-200x200">
-                    <img :src="rdj.imageUrl" alt="Image">
-                  </figure>
-                </div>
-                <div class="card-content">
-                  <div class="media">
-                    <div class="media-content">
-                      <p class="title name">{{ rdj.name }}</p>
+              <a :href="rdj.youtube || rdj.soundcloud" target="blank_">
+                <div class="card">
+                  <div class="card-image">
+                    <figure class="image is-200x200">
+                      <img :src="rdj.imageUrl" alt="Image">
+                    </figure>
+                  </div>
+                  <div class="card-content">
+                    <div class="media">
+                      <div class="media-content">
+                        <p class="title name">{{ rdj.name }}</p>
+                        <p class="subtitle location">{{ rdj.location }}</p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              </a>
             </div>
           </div>
         </div>
@@ -68,7 +71,19 @@ export default {
   padding: 0.5rem;
 }
 
+.title {
+  padding-bottom: 0;
+}
+
 .title.name {
   font-size: 1.1rem;
+}
+
+.subtitle.location {
+  font-size: 0.85rem;
+}
+
+.card:hover {
+  box-shadow: 1px 15px 15px #CCC;
 }
 </style>
