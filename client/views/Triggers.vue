05059f61bd984d8e4b1bf4cf8001a50dbc5df1fd
braincomb <zapper@gmail.com>
2017-05-28 23:30:04 -0700

    Add basic trigger display pulled from a JSON feed.

--- client/views/Triggers.vue
+++ client/views/Triggers.vue
@@ -17,7 +17,22 @@
     <section class="hero">
       <div class="hero-body">
         <div class="container">
-          <p>content here</p>
+          <table class="table is-striped">
+            <thead>
+              <tr>
+                <th class="column-keyword">Trigger</th>
+                <th class="column-content">Content</th>
+                <th class="column-username">Created By</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="trigger in triggers">
+                <td>{{ trigger.keyword }}</td>
+                <td>{{ trigger.content }}</td>
+                <td>{{ trigger.creator.username }}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </section>
@@ -28,12 +43,49 @@
 import axios from 'axios'
 
 export default {
-  name: 'Triggers'
+  name: 'Triggers',
+
+  data () {
+    return {
+      triggers: [],
+      errors: []
+    }
+  },
+
+  created() {
+    axios.get(`http://chilloutmusic.fm/triggers.json`)
+    .then(response => {
+      this.triggers = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
 }
 </script>
 
 <style scoped>
 .page-triggers {
   background-color: #d3beff;
+}
+
+.table {
+  table-layout: fixed;
+}
+
+.table td, .table th {
+  vertical-align: middle;
+}
+
+.column-trigger {
+  width: 20%;
+}
+
+.column-content {
+  width: 60%;
+}
+
+.column-username {
+  width: 20%;
 }
 </style>
