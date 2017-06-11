19f4ad3ec456a6eb669c7e6445bf3c9c6089f0ce
braincomb <zapper@gmail.com>
2017-06-10 21:09:43 -0700

    Added About page content.

--- client/views/About.vue
+++ client/views/About.vue
@@ -7,7 +7,7 @@
             <div class="column is-one-third">
               <h1 class="title" style="color: #000"><span class="icon is-large"><i class="fa fa-id-card-o"></i></span>&nbsp;About</h1>
               <h2 class="subtitle">
-                Meet the people behind Chillout Music
+                Meet the people behind Chillout Music.
               </h2>
             </div>
           </div>
@@ -17,7 +17,266 @@
     <section class="hero">
       <div class="hero-body">
         <div class="container">
-          <p>about content here</p>
+          <div class="content">
+            <blockquote>
+              <p>We are a small group of chillers from around the world. We manage a Plug.dj virtual DJ community on volunteer basis; we love chill music and want more people to learn about it. We would be delighted to invite you to our social music room on <a href="http://plug.dj/the-chillout-room">Plug.dj</a>.</p>
+              <p>If you wish to reach us, feel free to message us via any social medium, stop by on Discord, or drop us a line at <a href="mailto:contact@chilloutmusic.fm">contact@chilloutmusic.fm</a>.</p>
+            </blockquote>
+          </div>
+          <div class="columns is-multiline">
+            <div class="column is-3">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="/img/classic07b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">Molly</p>
+                      <p class="subtitle is-6">Host</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-3">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="/img/warrior07b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">Lizz-E</p>
+                      <p class="subtitle is-6">Co-Host</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-3">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="/img/diner-e02b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">Kris</p>
+                      <p class="subtitle is-6">Co-Host</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-3">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="/img/robot07b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">Cerebral Parsley</p>
+                      <p class="subtitle is-6">Co-Host</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-2">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image">
+                    <img src="/img/robot15b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-5">CaptainAnderz</p>
+                      <p class="subtitle is-6">Manager</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-2">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image">
+                    <img src="/img/base13b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-5">DnbJohnny</p>
+                      <p class="subtitle is-6">Manager</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-2">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image">
+                    <img src="/img/robot01b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-5">FishToast</p>
+                      <p class="subtitle is-6">Manager</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-2">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image">
+                    <img src="/img/robot02b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-5">Georgeo</p>
+                      <p class="subtitle is-6">Manager</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-2">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image">
+                    <img src="/img/robot01b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-5">Golden Bot</p>
+                      <p class="subtitle is-6">Manager</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-2">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image">
+                    <img src="/img/sea-t02b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-5">karmakazemonk</p>
+                      <p class="subtitle is-6">Manager</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-2">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image">
+                    <img src="/img/dragonp04b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-5">Mitch</p>
+                      <p class="subtitle is-6">Manager</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-2">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image">
+                    <img src="/img/zoo13b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-5">Powdered Toast Man</p>
+                      <p class="subtitle is-6">Manager</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-2">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image">
+                    <img src="/img/warrior09b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-5">Sexy Cherry</p>
+                      <p class="subtitle is-6">Manager</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="column is-2">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image">
+                    <img src="/img/beach-s07b.png" alt="Image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-5">ChilloutRoomba</p>
+                      <p class="subtitle is-6">Bot</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     </section>
@@ -33,5 +292,17 @@ export default {
 <style scoped>
 .page-about {
   background-color: #beffe7;
+}
+
+.image.is-4by3 img {
+  height: auto;
+  width: auto;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.subtitle:not(:last-child), .title:not(:last-child) {
+  margin-bottom: 1.0rem;
 }
 </style>
